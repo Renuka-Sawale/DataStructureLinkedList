2b66665a9249f84c0effa8b5b15456520087cccd
Renuka Sawale <rrsuryawanshi11@gmail.com>
2021-05-14 23:39:40 +0530

    Added linked list welcome message

---
+++ src/main/java/com/datastructure/MyNode.java
@@ -0,0 +1,27 @@
+package com.datastructure;
+
+class LinkedList {
+    public static void main(String[] agrs) {
+        System.out.println("Welcome to linked list");
+    }
+}
+
+/*
+public class MyNode<K> {
+    private K key;
+    private MyNode next;
+
+    public MyNode(K key) {
+        this.key = key;
+        this.next = null;
+    }
+
+    public MyNode getNext() {
+        return next;
+    }
+
+    public void setNext(MyNode next) {
+        this.next = next;
+    }
+}
+*/
