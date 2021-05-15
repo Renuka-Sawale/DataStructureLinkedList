2125cec515d821a428a3d3d242e53f404fd7201a
Renuka Sawale <rrsuryawanshi11@gmail.com>
2021-05-16 01:46:27 +0530

    Inserting new element to the linked list sequence

--- src/test/java/com/datastructure/MyLinkedListTest.java
+++ src/test/java/com/datastructure/MyLinkedListTest.java
@@ -97,4 +97,22 @@ public class MyLinkedListTest {
         System.out.println(result.getKey());
         Assertions.assertEquals(mySecondNode.getKey(),result.getKey());
     }
+
+    @Test
+    public void given4NumbersWhenInsertingNewNumberAfterTwoElementsShouldPassLinkedListResult() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myFourthNode = new MyNode<>(70);
+        MyNode<Integer> myThirdNode = new MyNode<>(40);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.append(mySecondNode);
+        myLinkedList.append(myThirdNode);
+        myLinkedList.insert(mySecondNode, myFourthNode);
+        myLinkedList.printMyNodes();
+        boolean result = myLinkedList.head.equals(myFirstNode) &&
+                         myLinkedList.head.getNext().equals(mySecondNode) &&
+                         myLinkedList.tail.equals(myThirdNode);
+        Assertions.assertTrue(result);
+    }
 }
